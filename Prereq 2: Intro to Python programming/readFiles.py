2803588fce95e8f42848496ff5a205c083097c94
Daniel Montes <63999930+DMontes98@users.noreply.github.com>
2023-09-06 10:32:02 -0500

    Add files via upload

---
+++ Prereq 2: Intro to Python programming/readFiles.py
@@ -0,0 +1,10 @@
+
+with open("python/sensor_data.txt", "r") as file:  # Open File
+    # now iterate over each line
+    for line in file:
+        # split the line into timestamp and temprature:
+        timestamp, temperature = line.strip().split(", ")
+
+        # now we can process the data
+        print("Timestamp: ", timestamp)
+        print("Temperature: ", temperature)
