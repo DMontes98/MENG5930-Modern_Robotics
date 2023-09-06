d6c61361a24b4112d469cbe5480e2639b7af22cc
hammadpervaiz <143550147+hammadpervaiz@users.noreply.github.com>
2023-09-06 10:39:24 -0500

    Add files via upload

---
+++ Prereq 2: Intro to Python programming/functions.py
@@ -0,0 +1,5 @@
+# Functions
+def add(a,b):                                           # Defining a function
+    sum = a+b
+    return sum                                          # Return statment
+print(add(3, -1)) 
